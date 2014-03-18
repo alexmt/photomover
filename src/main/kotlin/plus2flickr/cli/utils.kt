2b2a23bfd5d12f7541dc0197203b029323261e4b
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-03-17 21:40:02 -0700

    Remove jetty plugin config; start jetty manually instead

--- src/main/kotlin/plus2flickr/cli/utils.kt
+++ src/main/kotlin/plus2flickr/cli/utils.kt
@@ -1,4 +1,4 @@
-package plus2flickr
+package plus2flickr.cli.utils
 
 import org.kohsuke.args4j.CmdLineParser
 import org.kohsuke.args4j.CmdLineException
@@ -17,37 +17,6 @@ import org.kohsuke.args4j.Option
 import java.util.HashMap
 import com.flickr4java.flickr.photosets.Photoset
 import java.util.regex.Pattern
-
-fun main(args: Array<String>) {
-  val commands = mapOf(
-      "organize" to wrapOperation(::organize, OrganizeOptions()),
-      "upload" to wrapOperation(::upload, UploadOptions()))
-  if (args.size == 0) {
-    println("Please specify one of following commands:")
-    println(commands.keySet())
-  } else {
-    val command = commands[args[0]]
-    if (command == null) {
-      println("Command '${args[0]}' is not supported")
-    } else {
-      command(args.drop(1))
-    }
-  }
-}
-
-fun wrapOperation<T>(
-    operation: (options: T) -> Unit, defaultOptions: T): (args: List<String>) -> Unit {
-  return { args ->
-    val parser = CmdLineParser(defaultOptions)
-    try {
-      parser.parseArgument(args)
-    } catch (ex: CmdLineException) {
-      println(ex.getMessage())
-      println(parser.printUsage(System.err))
-    }
-    operation(defaultOptions)
-  }
-}
 
 data class OrganizeOptions() {
   Option("-s") var source: String? = null
---
+++ src/main/kotlin/plus2flickr/cli/web.kt
@@ -0,0 +1,25 @@
+package plus2flickr.cli.web
+
+import org.kohsuke.args4j.Option
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.ServletContextHandler
+import plus2flickr.guice.GuiceContext
+import com.google.inject.servlet.GuiceFilter
+import javax.servlet.DispatcherType
+import org.eclipse.jetty.servlet.ServletHandler
+import java.util.EnumSet
+
+
+data class StartWebOptions { Option("-p")  var port: Int = 8080 }
+
+fun start(options: StartWebOptions) {
+  val context = ServletContextHandler()
+  context.setContextPath("/services")
+  context.addEventListener(GuiceContext())
+  context.addFilter(javaClass<GuiceFilter>(), "/*", EnumSet.of(DispatcherType.REQUEST))
+
+  val server = Server(options.port)
+  server.setHandler(context)
+  server.start()
+  server.join()
+}
