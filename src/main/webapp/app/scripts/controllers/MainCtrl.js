35d8076d7aff0b540a6ff6b6887aa3f9f6077de1
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-03-26 21:41:48 -0700

    Albums should be shown automatically on page open and after service is linked

--- src/main/webapp/app/scripts/controllers/MainCtrl.js
+++ src/main/webapp/app/scripts/controllers/MainCtrl.js
@@ -2,18 +2,31 @@
 
 angular.module('controllers')
   .controller('MainCtrl', ['$scope', '$window', 'User', 'App', 'Google', function ($scope, $window, User, App, Google) {
-    $scope.userInfo = User.info();
-    $scope.switchTo = function(service) {
+
+    function applyUserInfo(info) {
+      $scope.userInfo = info;
+      for (var account in info.accountsState) {
+        if (info.accountsState[account]) {
+          $scope.showAlbums(account);
+        }
+      }
+    }
+
+    User.info(applyUserInfo);
+
+    $scope.showAlbums = function(service) {
       $scope.albums = User.albums(service);
     };
+
     $scope.signInToFlickr = function() {
       window.location.href = 'services/user/flickr/authorize';
     };
+
     $scope.signInToGoogle = function() {
       App.googleAppSettings(function(settings) {
         Google.authorize(settings).then(function(code) {
           User.authorizeGoogleAccount(code, function (response) {
-            $scope.userInfo = response.data;
+            applyUserInfo(response.data);
           });
         });
       });
