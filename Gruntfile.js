b8a1a32ef6b6438e14f4ce98c07533903432238d
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-06-14 23:58:06 -0700

    Don't copy bower_components to Grunt build directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -282,7 +282,6 @@ module.exports = function (grunt) {
               '.htaccess',
               '*.html',
               'views/{,*/}*.html',
-              'bower_components/**/*',
               'images/{,*/}*.{webp}',
               'fonts/*'
             ]
